39dc58abafb6a24a0b4aaf93496929f69275d023
Dmitry Zyryanov <cshlovjah@gmail.com>
2016-07-29 01:33:13 +0500

    Первый коммитgit add

--- public/js/libs/mainframe.js
+++ public/js/libs/mainframe.js
@@ -1,3 +1,21 @@
 /**
  * Created by cshlovjah on 28.07.16.
  */
+function mainFrame() {
+}
+
+//Очистка содержимого div
+mainFrame.prototype.clearFrame = function (framenameid) {
+    console.log(framenameid);
+    $('#'+framenameid).empty();
+};
+//переименовываем выбранный id
+mainFrame.prototype.renameFrame = function (framenameid, newframenameid) {
+
+    $('#'+framenameid).attr('id', newframenameid);
+};
+
+mainFrame.prototype.deleteFrame = function (framenameid, newframenameid) {
+    console.log(framenameid);
+   
+};
--- public/js/this/menu.js
+++ public/js/this/menu.js
@@ -1,3 +1,43 @@
 /**
  * Created by cshlovjah on 28.07.16.
  */
+var mainFrame = new mainFrame();
+//Администрирование
+$('.user-control').on('click', function () {
+    mainFrame.clearFrame('mainframe');
+    console.log("User control");
+});
+
+$('.boxes-control').on('click', function () {
+    console.log("boxes control");
+    mainFrame.renameFrame('mainframe','nonmainframe');
+});
+
+$('.posts-control').on('click', function () {
+    console.log("posts control");
+});
+
+$('.typeOfWork-control').on('click', function () {
+    console.log("Type of work control");
+});
+
+$('.customer-control').on('click', function () {
+    console.log("Customer control");
+});
+
+$('.log-control').on('click', function () {
+    console.log("Log control");
+});
+
+//Заявки
+$('.add-event').on('click', function () {
+    console.log("Add event control");
+});
+
+$('.update-event').on('click', function () {
+    console.log("Update event");
+});
+
+$('.delete-event').on('click', function () {
+    console.log("Delete event");
+});
